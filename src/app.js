2164d6a55b81bd8323957a173e18e5b5c1d9bd40
laplace1009 <laplace1009@gmail.com>
2024-04-18 21:48:23 +0900

    basic commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000);
